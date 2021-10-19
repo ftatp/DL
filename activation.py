43af7b7f3fbb33232f9456cab9ed19eb2f04aa90
ftatp <ftatp5901@gmail.com>
2021-10-19 23:42:17 +0900

    Set activation functions

---
+++ activation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def stair(x):
+    y = x > 0
+    return np.array(y, np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def identity(x):
+    return x
