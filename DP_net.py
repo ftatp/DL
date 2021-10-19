d9dc851b7996966910b8d66be777c650678a8ab3
ftatp <ftatp5901@gmail.com>
2021-10-19 23:41:25 +0900

    Construct Deep Learning Network

---
+++ DP_net.py
@@ -0,0 +1,57 @@
+import numpy as np
+import activation
+
+class Network():
+    def __init__(self):
+        network = {}
+        network['hidden_layers'] = []
+        network['bias'] = []
+
+        network['hidden_layers'].append(np.array([
+            [0.1, 0.3, 0.5],
+            [0.2, 0.4, 0.6]
+        ])) # input 층의 퍼셉트론 수 * output 층의 퍼셉트론 수
+        network['bias'].append(np.array([0.1, 0.2, 0.3])) # 다음 층의 퍼셉트론 수 만큼
+
+        network['hidden_layers'].append(np.array([
+            [0.1, 0.4],
+            [0.2, 0.5],
+            [0.3, 0.6]
+        ]))
+        network['bias'].append([0.1, 0.2])
+
+        network['hidden_layers'].append(np.array([
+            [0.1, 0.3],
+            [0.2, 0.4]
+        ]))
+        network['bias'].append(np.array([0.1, 0.2]))
+
+        self.depth = 3
+        self.network = network
+
+    def forward(self, x): # input layer values in np.array
+        hidden_layers = self.network['hidden_layers']
+        bias = self.network['bias']
+
+        depth = self.depth
+        input_layer = x
+
+        for i in range(depth - 1):
+            print("Depth : ", i + 1)
+            output_layer = np.dot(input_layer, hidden_layers[i]) + bias[i]
+            print("result : \n", output_layer)
+
+            output_layer = activation.sigmoid(output_layer)
+            print("After activation result : ", output_layer)
+            input_layer = output_layer
+            print("------------------------------------------------------------")
+    
+
+        print("Depth : ", depth)
+        output_layer = np.dot(input_layer, hidden_layers[depth - 1]) + bias[depth - 1]
+        print("result : \n", output_layer)
+        activation.identity(output_layer)
+        print("After activation result : ", output_layer)
+        print("------------------------------------------------------------")
+        return
+   
