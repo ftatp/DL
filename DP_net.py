357eda993bab0b82fa0d09abfeadd7f713e6a07d
ftatp <ftatp5901@gmail.com>
2021-10-20 00:58:48 +0900

    Add predict function

--- DP_net.py
+++ DP_net.py
@@ -1,33 +1,42 @@
 import numpy as np
 import activation
+import pickle
 
 class Network():
-    def __init__(self):
-        network = {}
-        network['hidden_layers'] = []
-        network['bias'] = []
+    def __init__(self, network_file=None):
+        if network_file != None:
+            print("network_file exists")
+            print("Loading...")
+            with open(network_file, 'rb') as f:
+                self.network = pickle.load(f)
 
-        network['hidden_layers'].append(np.array([
-            [0.1, 0.3, 0.5],
-            [0.2, 0.4, 0.6]
-        ])) # input 층의 퍼셉트론 수 * output 층의 퍼셉트론 수
-        network['bias'].append(np.array([0.1, 0.2, 0.3])) # 다음 층의 퍼셉트론 수 만큼
+        else:
+            print("Building network...")
+            network = {}
+            network['hidden_layers'] = []
+            network['bias'] = []
 
-        network['hidden_layers'].append(np.array([
-            [0.1, 0.4],
-            [0.2, 0.5],
-            [0.3, 0.6]
-        ]))
-        network['bias'].append([0.1, 0.2])
+            network['hidden_layers'].append(np.array([
+                [0.1, 0.3, 0.5],
+                [0.2, 0.4, 0.6]
+            ])) # input 층의 퍼셉트론 수 * output 층의 퍼셉트론 수
+            network['bias'].append(np.array([0.1, 0.2, 0.3])) # 다음 층의 퍼셉트론 수 만큼
 
-        network['hidden_layers'].append(np.array([
-            [0.1, 0.3],
-            [0.2, 0.4]
-        ]))
-        network['bias'].append(np.array([0.1, 0.2]))
+            network['hidden_layers'].append(np.array([
+                [0.1, 0.4],
+                [0.2, 0.5],
+                [0.3, 0.6]
+            ]))
+            network['bias'].append([0.1, 0.2])
 
-        self.depth = 3
-        self.network = network
+            network['hidden_layers'].append(np.array([
+                [0.1, 0.3],
+                [0.2, 0.4]
+            ]))
+            network['bias'].append(np.array([0.1, 0.2]))
+
+            self.depth = 3
+            self.network = network
 
     def forward(self, input_layer, activation_func, output_func): # input layer values in np.array
         hidden_layers = self.network['hidden_layers']
@@ -53,4 +62,7 @@ class Network():
         print("After activation result : ", output_layer)
         print("------------------------------------------------------------")
         return output_layer
-   
+  
+    def predict(self, input_layer, activation_func, output_func):
+        results = forward(input_layer, activation_func, output_func)
+        return np.argmax(results)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys, os
+sys.path.append(os.pardir)
+
 import numpy as np
 import DP_net
 import matplotlib.pylab as plt
 import activation
 
-net = DP_net.Network()
+
+#from dataset.mnist import load_mnist
+
+net = DP_net.Network("dataset/sample_weight.pkl")
 net.forward(np.array([1.0, 0.5]), activation.sigmoid, activation.identity)
