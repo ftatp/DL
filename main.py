754a3cdc2e820acdedfd2fd5934d6ab1c8de4f58
ftatp <ftatp5901@gmail.com>
2021-10-24 18:46:06 +0900

    Added gradient descent methodpython

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ import lossFuncs
 import deriative
 
 
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-
-print(x_train.shape, t_train.shape) # (60000, 784) , (60000, 10) 784 = 28*28
-print(x_test.shape, t_test.shape)   # (10000, 784) , (10000, 10)
-
-train_size = x_train.shape[0]
-batch_size = 10
-batch_mask = np.random.choice(train_size, batch_size)
-x_batch = x_train[batch_mask]
-t_batch = t_train[batch_mask]
-
+#(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+#
+#print(x_train.shape, t_train.shape) # (60000, 784) , (60000, 10) 784 = 28*28
+#print(x_test.shape, t_test.shape)   # (10000, 784) , (10000, 10)
+#
+#train_size = x_train.shape[0]
+#batch_size = 10
+#batch_mask = np.random.choice(train_size, batch_size)
+#x_batch = x_train[batch_mask]
+#t_batch = t_train[batch_mask]
+#
 #net = Network.Network("dataset/sample_weight.pkl")
 #net.forward(np.array([1.0, 0.5]), activation.sigmoid, activation.identity)
 
@@ -30,6 +30,6 @@ t_batch = t_train[batch_mask]
 def function_2(x):
     return x[0]**2 + x[1]**2
 
-grad = deriative.numerical_gradient(function_2, np.array([3, 4]))
-print(grad)
+minVal = deriative.gradient_descent(function_2, np.array([3.0, 4.0]))
+print(minVal)
 
